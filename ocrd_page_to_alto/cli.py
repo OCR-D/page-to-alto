76ab3b4133ca34af156700f5ea812bd390386f60
Konstantin Baierer <unixprog@gmail.com>
2021-03-31 11:29:16 +0200

    initLogging

--- ocrd_page_to_alto/cli.py
+++ ocrd_page_to_alto/cli.py
@@ -1,5 +1,6 @@
 import click
 from .convert import OcrdPageAltoConverter
+from ocrd_utils import initLogging
 
 @click.command()
 @click.argument('filename')
@@ -7,6 +8,7 @@ def main(filename):
     """
     Convert PAGE to ALTO
     """
+    initLogging()
     converter = OcrdPageAltoConverter(page_filename=filename)
     converter.convert()
     print(converter)

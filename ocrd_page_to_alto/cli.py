6b9bf28e1792a6bbc987e3a30e461649eae3a3c5
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2021-06-14 19:09:39 +0200

    add loglevel option

--- ocrd_page_to_alto/cli.py
+++ ocrd_page_to_alto/cli.py
@@ -1,8 +1,10 @@
 import click
 from .convert import OcrdPageAltoConverter
 from ocrd_utils import initLogging
+from ocrd.decorators import ocrd_loglevel
 
 @click.command()
+@ocrd_loglevel
 @click.option('--check-words/--no-check-words', default=True, help='Check whether PAGE-XML contains any Words and fail if not')
 @click.option('--check-border/--no-check-border', default=True, help='Check whether PAGE-XML contains Border or PrintSpace')
 @click.option('--skip-empty-lines/--no-skip-empty-lines', default=False, help='Whether to omit or keep empty lines in PAGE-XML')
@@ -12,7 +14,7 @@ from ocrd_utils import initLogging
 @click.option('--textequiv-index', default=0, help='If multiple textequiv, use the n-th TextEquiv by @index')
 @click.option('--textequiv-fallback-strategy', default='last', type=click.Choice(['raise', 'first', 'last']), help="What to do if nth textequiv isn't available. 'raise' will lead to a runtime error, 'first' will use the first TextEquiv, 'last' will use the last TextEquiv on the element")
 @click.argument('filename')
-def main(check_words, check_border, skip_empty_lines, trailing_dash_to_hyp, dummy_textline, dummy_word, textequiv_index, textequiv_fallback_strategy, filename):
+def main(log_level, check_words, check_border, skip_empty_lines, trailing_dash_to_hyp, dummy_textline, dummy_word, textequiv_index, textequiv_fallback_strategy, filename):
     """
     Convert PAGE to ALTO
     """

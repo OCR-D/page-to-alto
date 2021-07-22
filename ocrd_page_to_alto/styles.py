3c94cefc3c5912faeac68b9f855b564962b8e564
Uwe Hartwig <uwe.hartwig@bitsrc.info>
2021-07-22 10:11:13 +0200

    [app][fix] compare vs page _type

--- ocrd_page_to_alto/styles.py
+++ ocrd_page_to_alto/styles.py
@@ -122,7 +122,7 @@ class LayoutTagManager(TextStylesManager):
     def set_alto_tag_from_type(self, reg_alto, reg_page):
         typ = reg_page.get_type() if hasattr(reg_page, 'get_type') else None
         if typ:
-            if hasattr(reg_alto, 'TYPE'):
+            if hasattr(reg_page, 'type_'):
                 reg_alto.set('TYPE', typ)
             reg_alto.set('TAGREFS', self.get_id(label=typ))
 
